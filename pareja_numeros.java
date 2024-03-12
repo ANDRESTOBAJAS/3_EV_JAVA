5e34e08444568931eea4fc59cbedae49ad0c460b
Andrés Tobajas Embid <116067926+ANDRESTOBAJAS@users.noreply.github.com>
2024-03-12 09:34:06 +0100

    Create pareja_numeros.java

---
+++ pareja_numeros.java
@@ -0,0 +1,19 @@
+package miCodigo;
+
+import java.util.Scanner;
+
+public class ManejaParejaNumeros {
+
+   public static void main(String[] args) {
+       int num1;
+       int num2;
+       Scanner entrada = new Scanner(System.in);
+       System.out.println("\nNumero 1: ");
+       num1=entrada.nextInt();
+       System.out.println("Has escrito el número: "+num1);
+       
+       
+
+   }
+
+}
