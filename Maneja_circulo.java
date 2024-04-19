175e94d4c97658490c588cc0a952bb53bbb7d052
Andrés Tobajas Embid <116067926+ANDRESTOBAJAS@users.noreply.github.com>
2024-04-19 09:29:34 +0200

    Create Maneja_circulo.java

---
+++ Maneja_circulo.java
@@ -0,0 +1,14 @@
+package paquete1;
+
+public class ManejaCirculo {
+
+	   public static void main(String[] args) {
+	       System.out.println("Vamos a hacer un circulo");
+	       Circulo miCirculito;
+	       miCirculito=new Circulo(15,20,2);
+	       
+	       miCirculito.devuelveArea();
+
+	   }
+
+	}
