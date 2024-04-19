81f6f4cf851300bea3e88ee8e81b917d967fdd2e
Andrés Tobajas Embid <116067926+ANDRESTOBAJAS@users.noreply.github.com>
2024-04-19 09:31:00 +0200

    Create Cilindro.java

---
+++ Cilindro.java
@@ -0,0 +1,27 @@
+package Paquete2;
+
+public class Cilindro {
+    private int radio;
+    private int diametro;
+
+    public Cilindro(int radio, int diametro) {
+        this.radio = radio;
+        this.diametro = diametro;
+    }
+
+    public int getRadio() {
+        return radio;
+    }
+
+    public void setRadio(int radio) {
+        this.radio = radio;
+    }
+
+    public int getDiametro() {
+        return diametro;
+    }
+
+    public void setDiametro(int diametro) {
+        this.diametro = diametro;
+    }
+}
